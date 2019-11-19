e7068cf3604898697bb6573f6fe9082dd98d699f
Adam <tiagosaurus@live.com>
2019-11-19 09:07:19 -0500

    moved over testing repo

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const { watch, src, dest, series } = require('gulp');
+const postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssvariables = require('postcss-simple-vars'),
+      nestedcss = require('postcss-nested'),
+      cssimport = require('postcss-import')
+      browserSync = require('browser-sync').create()
+      mixins = require('postcss-mixins');
+
+
+function browSync() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    })
+}
+
+function html(cb) {
+    browserSync.reload();
+    cb();
+}
+
+function cssInject() {
+    return src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+}
+
+function css() {
+    return src('app/assets/styles/styles.css')
+        .pipe(postcss([cssimport, mixins, autoprefixer, cssvariables, nestedcss]))
+        .pipe(dest('./app/temp/styles'));
+}
+
+exports.default = () => {
+    browSync();
+    watch('./app/index.html', html);
+    watch('./app/assets/styles/**/*.css', series(css, cssInject));
+}
+
+
