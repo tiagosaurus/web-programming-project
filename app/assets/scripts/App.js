4c4740897c30c9233fdfad6161f1a0e2e61c3647
Adam <tiagosaurus@live.com>
2019-12-03 21:45:08 -0500

    Updated dropdown with select2

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,10 +2,21 @@ import MenuMobile from './modules/MenuMobile.js';
 import ScrollReveal from './modules/ScrollReveal.js';
 import Navbar from './modules/Navbar.js';
 import $ from 'jquery';
+import 'select2';   
 
 new MenuMobile();
 new ScrollReveal($(".what-we-do-item"), "85%");
 new ScrollReveal($(".about-us"), "65%");
 new ScrollReveal($(".world-map__country-links"), "85%");
 new ScrollReveal($(".text-scroll-reveal"), "85%");
-new Navbar();       
+new Navbar();  
+
+$(".dropdownOptions").select2({
+    placeholder: "Select a Country",
+    dropdownAutoWidth : true,
+    width: 'auto'
+});
+
+$(".dropdownOptions").change(function() {
+    window.location = $(".dropdownOptions").val();
+});
